d942f4df094de6715216f2ae2d2f53afc95b6213
Unknown <oriollloveraroura@gmail.com>
2017-05-04 17:33:00 +0200

    Arreglar Gestió Rols

--- vendor/friendsofsymfony/user-bundle/Form/Type/ProfileFormType.php
+++ vendor/friendsofsymfony/user-bundle/Form/Type/ProfileFormType.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the FOSUserBundle package.
  *
@@ -8,24 +7,22 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace FOS\UserBundle\Form\Type;
-
 use FOS\UserBundle\Util\LegacyFormHelper;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 class ProfileFormType extends AbstractType
 {
     /**
      * @var string
      */
     private $class;
-
     /**
      * @param string $class The User class name
      */
@@ -33,31 +30,36 @@ class ProfileFormType extends AbstractType
     {
         $this->class = $class;
     }
-
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $this->buildUserForm($builder, $options);
-
         $constraintsOptions = array(
-            'message' => 'fos_user.current_password.invalid',
-            );
-
+            'message' => 'Contrasenya incorrecte',
+        );
         if (!empty($options['validation_groups'])) {
             $constraintsOptions['groups'] = array(reset($options['validation_groups']));
         }
-
         $builder->add('current_password', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'), array(
-            'label' => 'form.current_password','attr'=>['class' => 'form-control'],
+            'label' => 'Contrasenya',
             'translation_domain' => 'FOSUserBundle',
             'mapped' => false,
             'constraints' => new UserPassword($constraintsOptions),
-            ));
-         //$this->addRolestoForm($builder);
+        ));
+        
+        $this->addRolesToForm($builder);
+        
     }
-
+    /**
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function addRolesToForm(FormBuilderInterface $builder){
+        return $builder->add('roles', ChoiceType::class, array('label' => 'Rol', 
+            'attr' => ['class' => 'select'],'multiple'  => false,
+            'required' => true, 'choices' => array("Administrador" => 'ROLE_ADMIN', "Usuari" => 'ROLE_USER', "Traductor" => 'ROLE_TRANS'), 'multiple' => true));
+    }
     /**
      * {@inheritdoc}
      */
@@ -68,9 +70,8 @@ class ProfileFormType extends AbstractType
             'csrf_token_id' => 'profile',
             // BC for SF < 2.8
             'intention' => 'profile',
-            ));
+        ));
     }
-
     // BC for SF < 3.0
     /**
      * {@inheritdoc}
@@ -79,7 +80,6 @@ class ProfileFormType extends AbstractType
     {
         return $this->getBlockPrefix();
     }
-
     /**
      * {@inheritdoc}
      */
@@ -87,7 +87,6 @@ class ProfileFormType extends AbstractType
     {
         return 'fos_user_profile';
     }
-
     /**
      * Builds the embedded form representing the user.
      *
@@ -97,28 +96,11 @@ class ProfileFormType extends AbstractType
     protected function buildUserForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('username', null, array('label' => 'form.username','attr'=>['class' => 'form-control'], 'translation_domain' => 'FOSUserBundle'))
-        ->add('cognom', null, array('label' => 'form.cognom', 'attr'=>['class' => 'form-control'],  'translation_domain' => 'FOSUserBundle'))
-        ->add('dni', null, array('label' => 'form.dni', 'attr'=>['class' => 'form-control'],  'translation_domain' => 'FOSUserBundle'))
-        ->add('image', null, array('label' => 'form.image','attr'=>['class' => 'form-control'], 'translation_domain' => 'FOSUserBundle'))
-        ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'attr'=>['class' => 'form-control'],'translation_domain' => 'FOSUserBundle'))
-       
-        /*->add('roles', null, array('label' => 'form.roles', 'translation_domain' => 'FOSUserBundle'))*/
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('cognom', null, array('label' => 'form.cognom', 'attr'=>['class' => 'form-control'],  'translation_domain' => 'FOSUserBundle'))
+            ->add('dni', null, array('label' => 'form.dni', 'attr'=>['class' => 'form-control'],  'translation_domain' => 'FOSUserBundle'))
+            ->add('image', null, array('label' => 'form.image','attr'=>['class' => 'form-control'], 'translation_domain' => 'FOSUserBundle'))
+            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
         ;
-
     }
-
-    /**
-    * @Security("has_role('ROLE_ADMIN')")
-    */
-    public function addRolestoForm(FormBuilderInterface $builder){
-        return $builder->add('roles', ChoiceType::class,array('label'=>'Rol',
-            'attr'=>['class' => 'selectRol'],
-            'required'=>true, 'choices'=> array("Traductor" =>'ROLE_TRANS',
-                "Administrador" =>'ROLE_ADMIN', "Usuari"=>'ROLE_USER')));
-    }
-
-
-
-
 }
